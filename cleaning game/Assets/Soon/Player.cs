ddbadea27981745a40c429ee8026ff0998527aab
tndmstla <jineung78@gmail.com>
2025-02-26 01:04:40 +0900

    [기능 추가] 플레이어 생성

---
+++ cleaning game/Assets/Soon/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    float hAxis;
+    float vAxis;
+    
+    Vector3 moveVec;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+}
